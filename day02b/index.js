947af4ef738d0178ea1a1d59ddedf26b0b6820e0
Mário Ferreira <m.j.v.ferreira@hotmail.com>
2023-12-03 15:23:05 +0000

    day02b

--- day02b/index.js
+++ day02b/index.js
@@ -1,19 +1,10 @@
 import { readFileSync } from "fs";
 
-const max = {
-  red: 12,
-  green: 13,
-  blue: 14,
-}
-
 const possibleCubes = readFileSync("input.txt", { encoding: "utf-8" })
   .split(/Game \d+: |\n/)
   .filter(line => line !== "")
-  .map(line => {
-    const cubes = {red: 0, green: 0, blue: 0};
-    line.split("; ").map(s => s.split(", ").map(sc => cubes[sc.split(" ")[1]] = Math.max(cubes[sc.split(" ")[1]], parseInt(sc.split(" ")[0]))));
-    return cubes.red * cubes.green * cubes.blue;
-  })
-  .reduce((a, b) => a+b)
+  .map(line => line.split("; ").map(s => s.split(", ").reduce((a, b) => ({ ...a, [b.split(" ")[1]]: Number(b.split(" ")[0]) }), {}))
+  .reduce((a, b) => { Object.keys(b).forEach(c => { a[c] = Math.max(a[c] || 0, b[c]) }); return a; }, {}))
+  .reduce((a,b) => a + Object.values(b).reduce((c,d) => c*d, 1), 0);
 
 console.log("RESULT: ", possibleCubes);
